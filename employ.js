4f313d17b7b17bd191809a3dd965d6f1019c1576
Yana Yakubchik <Yana Yakubchik>
2016-06-11 11:33:49 -0400

    I started working on the JS, just checking that everything is loading

---
+++ employ.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+
+// Link to Firebase
+var dataRef = new Firebase("https://ebay-clone-rcb.firebaseio.com/");
+
+console.log ('hi');
+
+$("submit").on("click", function() {
+  console.log('hello');
+  })
+
+}
